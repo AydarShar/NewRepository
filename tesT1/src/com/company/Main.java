7366ad759fc754997a80910c7f957f4c2f21bb3d
AydarShar <79001710+AydarShar@users.noreply.github.com>
2022-03-21 16:29:06 +0300

    Add files via upload

---
+++ tesT1/src/com/company/Main.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {107, 6, -17, 43, 24, 35, 98};
+        int[] arr1 = {107, 6, -17, 43, 24, 35, 98};
+        int[][] arr10 = {{1, 2, 3}, {5, 7, 8}};
+        int[][] arr11 = {{1, 2, 3}, {5, 7, 8}};
+
+//        System.out.println("toString - преобразует массив в строку: " + Arrays.toString(arr));
+
+//        Arrays.sort(arr);
+//        System.out.println("sort - сортирует массив от меньшего значения к большему: " + Arrays.toString(arr));
+
+//        System.out.println("binarySearch - ищет в массиве указанное значение, используя алгоритм двоичного поиска: " + Arrays.binarySearch(arr,6));
+
+//        System.out.println("compare - сравнивает два массива лексикографически: " + Arrays.compare(arr, arr1));
+//
+//        System.out.println("compareUnsigned - cравнивает два массива лексикографически, численно рассматривая элементы как беззнаковые: " + Arrays.compareUnsigned(arr, arr1));
+
+//
+//        int [] arrCopy = Arrays.copyOf(arr,5);
+//        System.out.println("copy Of - копирует значения массива в новый массив, можно выборочно: " + Arrays.toString(arrCopy));
+//
+//        int [] arrCopy = Arrays.copyOfRange(arr,2,5);
+//        System.out.println("copyOfRange - копирует указанный диапазон указанного массива в новый массив: " + Arrays.toString(arrCopy));
+//
+//        System.out.println("equals - сравнивает содержимое двух массивов  " + Arrays.equals(arr, arr1));
+
+//        System.out.println("deepEquals - возвращает true, если содержимое двух многомерных массивов равны друг другу: " + Arrays.deepEquals(arr10, arr11));
+//
+//        System.out.println("deepToString - возвращает строковое представление многомерного массива:  " + Arrays.deepToString(arr10));
+//
+//        System.out.println("deepHashCode - возвращает хэш-код на основе содержимого многомерного массива:  " + Arrays.deepHashCode(arr10));
+
+//        System.out.println("hashCode - возвращает хэш-код на основе содержимого массива:  " + Arrays.hashCode(arr));
+
+//        Arrays.fill(arr1, 3);
+//        System.out.println("fill - присваивает указанное значение каждому элементу массива: " + Arrays.toString(arr1));
+
+//        System.out.println("mismatch - находит и возвращает индекс первого несоответствия между двумя массивами, в противном случае возвращает -1: " + Arrays.mismatch(arr, arr1));
+
+//        Arrays.parallelPrefix(arr, (x, y) -> x + y);
+//        System.out.println("parallelPrefix - вычисляет некоторое значение для элементов массива (например, сумму элементов):  " + Arrays.toString(arr));
+
+//        Arrays.parallelSort(arr1);
+//        System.out.println("parallelSort - сортирует массив:  " + Arrays.toString(arr1));
+
+//        Arrays.parallelSetAll(arr1, i -> i * 5);
+//        System.out.println("parallelSort - устанавливает элементы массива с помощью лямбда-выражения:  " + Arrays.toString(arr1));
+
+//        Arrays.setAll(arr, i -> i * 2);
+//        System.out.println("setAll - устанавливает все элементы массива функцией, которая вычисляет каждый элемент: " + Arrays.toString(arr));
+//
+//        Arrays.spliterator(arr);
+//        System.out.println("spliterator - это итератор для навигации по отдельным элементам, названия, поддерживает разделение и итерацию вместе: " + Arrays.toString(arr));
+//
+//        Arrays.stream(arr);
+//        System.out.println("steam - возвращает последовательность элементов массива, над которой могут выполняться агрегатные операции: " + Arrays.toString(arr));
+    }
+}
---
+++ tesT2/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        long c = 1234_5678_9012_3456L;
+        // long s = 999_99_9999_L;
+        float p =  3.14_15F;
+        // float v =  _1.21_15F;
+        int x = 12_3 + 3_2_1;
+        // int y = 1_23 + 2_1_1_;
+
+        System.out.println("c = " + c);
+        // System.out.println("s = " + s);
+        System.out.println("p = " + p);
+        // System.out.println("v = " + v);
+        System.out.println("x = " + x);
+        // System.out.println("y = " + y);
+    }
+}
